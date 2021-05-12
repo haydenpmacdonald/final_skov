3e3a885ad5771043accbf721ba66836ca4393e60
Hayden MacDonald <haydenpmacdonald@gmail.com>
2021-05-12 16:21:10 -0700

    slider issues

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,17 +1,9 @@
 import React from 'react'
-// import { Carousel } from 'react-bootstrap'
 import '../styles/Slider.css';
-// import image1 from '../header.jpeg'
-// import image2 from '../header3.jpeg'
-// import image3 from '../header4.jpeg'
 import { Link } from 'react-router-dom';
 
 const Slider = () => {
-  // const [index, setIndex] = useState(0);
 
-  // const handleSelect = (selectedIndex, e) => {
-  //   setIndex(selectedIndex);
-  // };
     return (
         <div className='carousel--cont'>
           <div className='temporary--carousel--section'>
@@ -25,40 +17,6 @@ const Slider = () => {
           </div> 
          
           </div>
-          {/* <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-        <img
-          className=" slider-- d-block w-100"
-          src={image1}
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3 className='bio--header'>Skov Music</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="slider--image d-block w-100"
-          src={image2}
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3 className='bio--header'>Skov Music</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="slider--image d-block w-100"
-          src={image3}
-          alt="Third slide"
-        />
-
-        <Carousel.Caption>
-          <h3 className='bio--header'>Skov Music</h3>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel> */}
         </div>
     )
 }
